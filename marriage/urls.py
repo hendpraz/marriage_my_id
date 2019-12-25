04063f8b6927d18a6adea10dd28f31a86e332933
hendpraz <mhendryp99@gmail.com>
2019-12-25 17:35:16 +0700

    Finished backend

--- marriage/urls.py
+++ marriage/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('tania-dika/', views.tania_dika_index, name='tania_dika'),
-    path('tania-dika/<str:guest_name>', views.tania_dika_invitation, name='tania_dika'),
+    path('tania-dika/<int:param_id>/<str:param_guest_name>', views.tania_dika_invitation, name='tania_dika_invitation'),
+    path('tania-dika/submit-message/<int:param_id>/<str:param_guest_name>', views.tania_dika_submit_message, name='submit_message'),
 ]
--- marriage/views.py
+++ marriage/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from marriage.models import Guest
+from django.http import Http404
+from django.http import HttpResponseRedirect
+
+# GLOBAL VAR
+
+dbname = 'db.sqlite3'
 
 # MAIN WEB PAGES
 
@@ -8,8 +16,45 @@ def index(request):
 
 # CLIENT WEB PAGES
 
-def tania_dika_index(request):
-    return render(request, 'marriage/tania-dika/index.html')
+def get_latest_messages():
+    return Guest.objects.exclude(message='').order_by('pub_date')
 
-def tania_dika_invitation(request, guest_name):
-    return render(request, 'marriage/tania-dika/index.html')
+
+def get_guest_name(param_id, param_guest_name):
+    try:
+        g = Guest.objects.get(id=param_id, guest_name=param_guest_name)
+    except Guest.DoesNotExist:
+        raise Http404("Guest with combination of id:%s and name:%s does not exist" % (str(param_id), str(param_guest_name)))
+    return g.guest_name
+
+
+def tania_dika_index(request):
+    template = loader.get_template('marriage/tania-dika/index.html')
+    context = {
+        'latest_messages' : get_latest_messages(),
+        'guest_name' : False
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def tania_dika_invitation(request, param_id, param_guest_name):
+    template = loader.get_template('marriage/tania-dika/index.html')
+    context = {
+        'latest_messages' : get_latest_messages(),
+        'guest_name' : get_guest_name(param_id, param_guest_name),
+        'guest_id' : param_id,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def tania_dika_submit_message(request, param_id, param_guest_name):
+    if(request.method == 'POST'):
+        try:
+            g = Guest.objects.get(id=param_id, guest_name=param_guest_name)
+            g.guest_role = request.POST['guest_role']
+            g.message = request.POST['message']
+            g.save()
+        except Guest.DoesNotExist:
+            raise Http404("Guest with combination of id:%s and name:%s does not exist" % (str(param_id), str(param_guest_name)))
+        
+    return HttpResponseRedirect('/tania-dika/%s/%s#notes_section' % (str(param_id), str(param_guest_name)))
