e2666d137e29cb4468fb61657db5501241016100
hendpraz <mhendryp99@gmail.com>
2019-12-29 12:54:34 +0700

    Update DEBUG in settings.py

--- marriage_my_id/settings.py
+++ marriage_my_id/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&h*lqtrc85uzu@9c@i-d!%d7+99+fpo^qylr^)8v8#4-abjp5_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
